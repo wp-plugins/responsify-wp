e8577e2f6496a31f0f6f1b8e8df6b166e84c03ce
stefanledin <stefanledin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-26 05:06:02 +0000

    tagging version 1.5.0
    git-svn-id: https://plugins.svn.wordpress.org/responsify-wp/tags/1.5.0@972897 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/views/settings.php
@@ -0,0 +1,15 @@
+<div class="wrap">
+	<h2>Responsify WP</h2>
+	<form method="post" action="options.php">
+		<?php 
+		settings_fields( 'responsify-wp-settings' );
+		do_settings_sections( 'responsify-wp-settings' );
+		
+		include 'globally_active.php';
+		
+		include 'selected_element.php';
+
+		include 'selected_sizes.php';
+		?>
+	</form>
+</div>
