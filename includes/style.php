92bd9d3c3b959feb2933329810ef0d74ae841d97
stefanledin <stefanledin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-07 18:47:21 +0000

    tagging version 1.6.1
    git-svn-id: https://plugins.svn.wordpress.org/responsify-wp/tags/1.6.1@1003438 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/style.php
@@ -0,0 +1,28 @@
+<?php
+class Style extends Create_Responsive_image
+{
+	public $style;
+
+	public function __construct($id, $settings)
+	{
+		parent::__construct($id, $settings);
+		$this->style = $this->buildCss($settings['selector']);
+	}
+
+	protected function buildCss($selector)
+	{
+		$css = '<style>';
+			$css .= $selector . ' {';
+				$css .= 'background-image: url("'.$this->images[0]['src'].'");';
+			$css .= '}';
+			for ($i=1; $i < count($this->images); $i++) { 
+				$css .= '@media screen and ('.$this->images[$i]['media_query'].') {';
+					$css .= $selector . '{';
+						$css .= 'background-image: url("'.$this->images[$i]['src'].'");';
+					$css .= '}';
+				$css .= '}';
+			}
+		$css .= '</style>';
+		return $css;
+	}
+}
