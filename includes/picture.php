309171d6af62366d93aee9ca59d1d651f2c458af
stefanledin <stefanledin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-02 12:49:02 +0000

    tagging 1.7.2
    git-svn-id: https://plugins.svn.wordpress.org/responsify-wp/tags/1.7.2@1103253 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/picture.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * A factory class which creates an instance of the proper class.
+ */
+class Picture
+{
+    /**
+     * Returns markup.
+     *
+     * @param $type
+     * @param $id
+     * @param null $settings
+     * @return string
+     */
+    public static function create ( $type, $id, $settings = null )
+	{
+        $native_mode = ( get_option( 'rwp_picturefill', 'on' ) == 'off' ) ? true : false;
+
+		switch ( strtolower($type) ) {
+            case 'attributes':
+                $responsive_image = self::create( $settings['element'], $id, array( 'output' => 'attributes' ) );
+                return $responsive_image;
+                break;
+
+            case 'span':
+                $responsive_image = new Span( $id, $settings );
+                break;
+            case 'img':
+                if ( $native_mode ) {
+                    $responsive_image = new Native_Img( $id, $settings );
+                } else {
+                    $responsive_image = new Img( $id, $settings );
+                }
+                break;
+			case 'element':
+			case 'picture':
+                if ( get_option('selected_element') == 'span' ) {
+                    $responsive_image = new Span( $id, $settings );
+                } else {
+                    if ( $native_mode ) {
+                        $responsive_image = new Native_Element( $id, $settings );
+                    } else {
+                        $responsive_image = new Element( $id, $settings );
+                    }
+                }
+				break;
+			case 'style':
+				$responsive_image = new Style( $id, $settings );
+				return $responsive_image->style;
+				break;
+		}
+        
+        if ( isset($settings['output']) && $settings['output'] == 'attributes' ) {
+            return $responsive_image->attributes;
+        }
+        return $responsive_image->markup;
+	}
+}
